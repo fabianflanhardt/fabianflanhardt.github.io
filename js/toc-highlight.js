45a2f5badf96a2fc39419b8f07e6423ce604f95e
Fabian Flanhardt <fabian.flanhardt@ceterion.com>
2018-04-28 01:24:03 +0200

    toc highlighting added

---
+++ js/toc-highlight.js
@@ -0,0 +1,55 @@
+
+// highlight first toc item after page load
+$(document).ready(function(){
+  // Highlight first toc item
+  var $headers = $('.clickable-header');
+  var firsttocid = $($headers[0]).attr('id');
+  var ListNode = $('[href="#'+ firsttocid +'"]').parent()
+  ListNode.addClass('active');
+});
+
+// query active section when scrolling and highlight items
+$(document).ready(function(){
+    // $sections incleudes all of the container divs that relate to menu items.
+    var $headers = $('.clickable-header'); 
+    
+    // The user scrolls
+    $(window).scroll(function(){
+      
+      // currentScroll is the number of pixels the window has been scrolled
+      var currentScroll = $(this).scrollTop();
+      
+      // $currentSection is somewhere to place the section we must be looking at
+      var id
+      
+      // We check the position of each of the divs compared to the windows scroll positon
+      $headers.each(function(){
+        // divPosition is the position down the page in px of the current section we are testing      
+        var divPosition = $(this).offset().top;
+        
+        // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
+        // the -1 is so that it includes the div 1px before the div leave the top of the window.
+        if( divPosition - 25 < currentScroll ){
+          // We have either read the section or are currently reading the section so we'll call it our current section
+          id = $(this).attr('id');
+          
+          // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
+        }
+        
+
+        
+      })
+
+      // keep first item highlighted as long as there is no other result
+      if (!id) {
+        id = $($headers[0]).attr('id');
+      }
+
+      // This is the bit of code that uses the currentSection as its source of ID
+      //var id = $currentSection.attr('id');
+      $('li').removeClass('active');
+      var ListNode = $('[href="#'+ id +'"]').parent()
+      ListNode.addClass('active');
+  
+    });
+  });
