8886eb5997cd02a62470875f395d523097cdf90c
Fabian Flanhardt <fflanhardt@bright-skies.de>
2019-05-04 22:51:15 +0200

    max width does not work with fixed in css, must be set in js

--- js/scroll-position-fixed.js
+++ js/scroll-position-fixed.js
@@ -1,10 +1,13 @@
-$(document).ready(function() {
-    $(document).scroll(function() {
-      var top = $(document).scrollTop();
-      var viewport = $("#sidebar-right")[0].offsetTop;
-        if (top >= viewport ) {
-            $('#toc-right').addClass("fixed");
-        } else if ($('#toc-right').hasClass('fixed')) {
-        $('#toc-right').removeClass('fixed')}
-    });
+$(document).ready(function () {
+  $(document).scroll(function () {
+    var top = $(document).scrollTop();
+    var viewport = $("#sidebar-right")[0].offsetTop;
+    var parentwidth = $('#sidebar-right').width();
+    if (top >= viewport) {
+      $('#toc-right').addClass("fixed").width(parentwidth);
+    }
+    else if ($('#toc-right').hasClass('fixed')) {
+      $('#toc-right').removeClass('fixed')
+    }
   });
+});
